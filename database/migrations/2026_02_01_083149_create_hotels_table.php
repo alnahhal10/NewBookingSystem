0be1bd04f074f268a1bde6e1e6d081f86c37e194
mohnahhal <alnhal10@gmail.com>
2026-02-04 09:11:40 +0200

    Add BookingPal Laravel project files

--- database/migrations/2026_02_01_083149_create_hotels_table.php
+++ database/migrations/2026_02_01_083149_create_hotels_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('hotels', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state')->nullable();
+            $table->string('country');
+            $table->string('postal_code')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('email')->nullable();
+            $table->text('description')->nullable();
+            
             $table->timestamps();
         });
     }
